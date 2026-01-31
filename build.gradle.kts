fb6bafc157ca8fdc9c30196973b748f469ddf1cb
jeyong <joon6093@naver.com>
2026-02-01 02:11:09 +0900

    Update Spring Boot plugin version to 4.0.2 and remove snapshot repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.0"
     kotlin("plugin.spring") version "2.3.0"
-    id("org.springframework.boot") version "4.1.0-SNAPSHOT"
+    id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.3.0"
 }
@@ -17,7 +17,6 @@ java {
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
