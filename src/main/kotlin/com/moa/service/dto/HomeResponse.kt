87fac2b109a997c6685d552a64e0021832972a87
jeyong <joon27216@gmail.com>
2026-02-22 20:26:53 +0900

    Remove Schema annotations for nullable fields in response data classes

--- src/main/kotlin/com/moa/service/dto/HomeResponse.kt
+++ src/main/kotlin/com/moa/service/dto/HomeResponse.kt
@@ -2,20 +2,16 @@ package com.moa.service.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.moa.entity.DailyWorkScheduleType
-import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalTime
 
 data class HomeResponse(
-    @field:Schema(nullable = true)
     val workplace: String?,
     val workedEarnings: Int,
     val standardSalary: Int,
     val dailyPay: Int,
     val type: DailyWorkScheduleType,
-    @field:Schema(nullable = true)
     @field:JsonFormat(pattern = "HH:mm")
     val clockInTime: LocalTime?,
-    @field:Schema(nullable = true)
     @field:JsonFormat(pattern = "HH:mm")
     val clockOutTime: LocalTime?,
 )
--- src/main/kotlin/com/moa/service/dto/OnboardingStatusResponse.kt
+++ src/main/kotlin/com/moa/service/dto/OnboardingStatusResponse.kt
@@ -1,13 +1,8 @@
 package com.moa.service.dto
 
-import io.swagger.v3.oas.annotations.media.Schema
-
 data class OnboardingStatusResponse(
-    @field:Schema(nullable = true)
     val profile: ProfileResponse?,
-    @field:Schema(nullable = true)
     val payroll: PayrollResponse?,
-    @field:Schema(nullable = true)
     val workPolicy: WorkPolicyResponse?,
     val hasRequiredTermsAgreed: Boolean,
 )
