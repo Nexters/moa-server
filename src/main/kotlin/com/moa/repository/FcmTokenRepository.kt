9c05c4bb54f65b9375f878fd904072c1721e188f
hod <100784510+subsub97@users.noreply.github.com>
2026-02-25 22:36:25 +0900

    Add logging to notification batch schedulers and make FcmTokenReposit… (#33)

--- src/main/kotlin/com/moa/repository/FcmTokenRepository.kt
+++ src/main/kotlin/com/moa/repository/FcmTokenRepository.kt
@@ -2,10 +2,13 @@ package com.moa.repository
 
 import com.moa.entity.FcmToken
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
 
 interface FcmTokenRepository : JpaRepository<FcmToken, Long> {
     fun findAllByMemberId(memberId: Long): List<FcmToken>
     fun findByToken(token: String): FcmToken?
+
+    @Transactional
     fun deleteByToken(token: String)
     fun findAllByMemberIdIn(memberIds: Collection<Long>): List<FcmToken>
     fun deleteAllByMemberId(memberId: Long)
