cfbb5efbb5b43aa5be458de9a48bdd2d5203488f
jeyong <joon27216@gmail.com>
2026-03-21 17:37:57 +0900

    Add joinedAt field to CalendarResponse and update CalendarController to fetch member join date

--- src/main/kotlin/com/moa/service/dto/CalendarResponse.kt
+++ src/main/kotlin/com/moa/service/dto/CalendarResponse.kt
@@ -1,6 +1,9 @@
 package com.moa.service.dto
 
+import java.time.LocalDate
+
 data class CalendarResponse(
     val earnings: MonthlyEarningsResponse,
     val schedules: List<WorkdayResponse>,
+    val joinedAt: LocalDate,
 )
