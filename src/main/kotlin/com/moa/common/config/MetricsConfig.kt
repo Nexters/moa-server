9cbeb5872211f3e5f61fb2e60bbb1e90b4ca0895
hod <godqhr721@gmail.com>
2026-05-03 20:00:13 +0900

    feat: TimedAspect bean 추가

--- src/main/kotlin/com/moa/common/config/MetricsConfig.kt
+++ src/main/kotlin/com/moa/common/config/MetricsConfig.kt
@@ -1,12 +1,15 @@
 package com.moa.common.config
 
+import io.micrometer.core.aop.TimedAspect
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 class MetricsConfig(
     @Value("\${spring.application.name}") private val appName: String,
     @Value("\${app.deploy-color}") private val deployColor: String,
@@ -20,4 +23,7 @@ class MetricsConfig(
             "app_version", appVersion,
         )
     }
+
+    @Bean
+    fun timedAspect(registry: MeterRegistry) = TimedAspect(registry)
 }
